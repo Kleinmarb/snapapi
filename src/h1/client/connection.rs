5522eaf35df2bef6486049a1f5027b57309a31fb
Kleinmarb <selimkentel@gmail.com>
2023-10-15 18:06:09 +0200

    client added not done yet

--- src/h1/client/connection.rs
+++ src/h1/client/connection.rs
@@ -1,3 +1,8 @@
-pub(crate) fn request_server() {
+use std::io::Write;
+use std::net::TcpStream;
 
+pub(crate) fn request_server(uri: &str) {
+   let stream = TcpStream::connect(uri).expect("uri is not available");
+
+    stream.write()
 }
