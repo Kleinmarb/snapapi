2e41a8c5bf9d05656d7dd14eff6126fe3e2a19b7
Kleinmarb <selimkentel@gmail.com>
2023-10-16 18:25:43 +0200

    even better client

--- src/h1/client/connection.rs
+++ src/h1/client/connection.rs
@@ -1,9 +1,12 @@
 use std::io::{Read, Write};
 use std::net::TcpStream;
 
-pub(crate) fn request_server(uri: &str) -> String {
-    let mut stream = TcpStream::connect(uri).expect("uri is not available");
-    stream.write(b"GET / HTTP/1.1\r\n\r\n").unwrap();
+pub(crate) fn request_server(_uri: &str) -> String {
+    let route = "/"; // Extract from uri
+    let host = "127.0.0.1:7878"; // Extract from uri
+
+    let mut stream = TcpStream::connect(host).expect("uri is not available");
+    stream.write(format!("GET {route} HTTP/1.1\r\nHost: {host}\r\n").as_bytes()).unwrap();
 
     let response = get_response(stream);
     response
@@ -12,5 +15,7 @@ pub(crate) fn request_server(uri: &str) -> String {
 fn get_response(mut stream: TcpStream) -> String {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    String::from_utf8_lossy(&buffer[..]).to_string()
+
+    let response = String::from_utf8_lossy(&buffer[..]).to_string();
+    response // Format for the end user before returning
 }
