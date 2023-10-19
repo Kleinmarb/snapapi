c4de667513904754385f32ca7ae4c4c7d95644e6
Kleinmarb <selimkentel@gmail.com>
2023-10-19 18:21:56 +0200

    remove unused attributes

--- src/h1/http.rs
+++ src/h1/http.rs
@@ -4,13 +4,11 @@ pub type QueryParams = FxHashMap<String, String>;
 pub(crate) type Handler = fn(QueryParams) -> Response;
 pub(crate) type Routes = FxHashMap<String, Handler>;
 
-#[allow(dead_code)]
 pub enum Response {
     Plain(String),
     StatusCode(String, String),
 }
 
-#[allow(unused)]
 impl Response {
     pub fn from_status_code(status_code: u16, detail: &str) -> Self {
         Self::StatusCode(
